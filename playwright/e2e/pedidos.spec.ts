3d8c5766e5df5345bcdc07e14625ec4ef07569b6
Otávio Carneiro Ribeiro <otavio@kyte.com.br>
2026-01-22 16:23:58 -0300

    Adicionando formato AAA

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -1,27 +1,27 @@
 import { test, expect } from '@playwright/test';
 
+// AAA - Arrange, Act, Assert - Preparar, Agir, Verificar
+
 test('deve consultar um pedido aprovado', async ({ page }) => {
+    
+    //Arrange
     await page.goto('http://localhost:5173/');
-
-    //Checkpoint
+    //Checkpoint (faz parte do Arrange)
     await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint');
-
-    //Ação
     //a[text()='Consultar Pedido']
     //a[href='/lookup']
     await page.getByRole('link', { name: 'Consultar Pedido' }).click();
-
-    //Checkpoint
     await expect(page.getByRole('heading')).toContainText('Consultar Pedido');
 
+    //Act
     await page.getByTestId('search-order-id').click();
     await page.getByTestId('search-order-id').fill('VLO-T0PGRW');
-
     await page.getByTestId('search-order-button').click();
 
+    //Assert
     await expect(page.getByTestId('order-result-id')).toBeVisible();
     await expect(page.getByTestId('order-result-id')).toContainText('VLO-T0PGRW');
-    
+
     await expect(page.getByTestId('order-result-status')).toBeVisible();
     await expect(page.getByTestId('order-result-status')).toContainText('APROVADO');
 });
