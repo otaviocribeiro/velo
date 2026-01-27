e3393e8e8dfe96bee4e04e807d094fb0858f0f04
Otávio Carneiro Ribeiro <otavio@kyte.com.br>
2026-01-26 21:44:07 -0300

    Desafio: Sobreviva sem Data Test IDs

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -21,14 +21,22 @@ test('deve consultar um pedido aprovado', async ({ page }) => {
     await expect(page.getByRole('heading')).toContainText('Consultar Pedido');
 
     //Act
-    await page.getByTestId('search-order-id').click();
-    await page.getByTestId('search-order-id').fill('VLO-T0PGRW');
-    await page.getByTestId('search-order-button').click();
+    //await page.getByTestId('search-order-id').click();
+    //await page.getByTestId('search-order-id').fill('VLO-T0PGRW');
+
+    // como seria o mesmo xpath - //label[text()='Número do Pedido']/../input o playwright deixa mais legivel 
+    await page.getByRole('textbox', { name: 'Número do Pedido' }).click();
+    await page.getByRole('textbox', { name: 'Número do Pedido' }).fill('VLO-T0PGRW');
+
+    //await page.getByTestId('search-order-button').click();
+    await page.getByRole('button', { name: 'Buscar Pedido' }).click();
 
     //Assert
-    await expect(page.getByTestId('order-result-id')).toBeVisible();
-    await expect(page.getByTestId('order-result-id')).toContainText('VLO-T0PGRW');
+    // await expect(page.getByTestId('order-result-id')).toBeVisible({timeout: 10000});
+    // await expect(page.getByTestId('order-result-id')).toContainText('VLO-T0PGRW');
+    await expect(page.getByText('VLO-T0PGRW')).toBeVisible();
 
-    await expect(page.getByTestId('order-result-status')).toBeVisible();
-    await expect(page.getByTestId('order-result-status')).toContainText('APROVADO');
+    // await expect(page.getByTestId('order-result-status')).toBeVisible();
+    // await expect(page.getByTestId('order-result-status')).toContainText('APROVADO');
+    await expect(page.getByText('APROVADO')).toBeVisible();
 });
