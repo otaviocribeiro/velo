3cacf1c615e264d82bc4e07e68fed45f690c18fd
Otávio Carneiro Ribeiro <otavio@kyte.com.br>
2026-03-24 18:10:22 -0300

    Aula de Testando estados de erro e mensagens de feedback finalizada

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -84,3 +84,34 @@ test('deve consultar um pedido aprovado', async ({ page }) => {
     await expect(page.getByTestId('order-result-status')).toBeVisible();
     await expect(page.getByTestId('order-result-status')).toContainText('APROVADO');
 });
+
+test('deve exibir mensagem quando o pedido não é encontrado', async ({ page }) => {
+
+    //Test Data
+    const order = 'VLO-123'
+    
+    //Arrange
+    await page.goto('http://localhost:5173/');
+    await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint');
+
+    await page.getByRole('link', { name: 'Consultar Pedido' }).click();
+    await expect(page.getByRole('heading')).toContainText('Consultar Pedido');
+
+    //Act
+    await page.getByRole('textbox', { name: 'Número do Pedido' }).fill(order);
+    await page.getByRole('button', { name: 'Buscar Pedido' }).click();
+
+    //Assert
+    //await expect(page.getByRole('heading', { name: 'Pedido não encontrado', level: 3})).toBeVisible();
+
+    //await expect(page.getByText('Verifique o número do pedido e tente novamente')).toBeVisible();
+    //const message =  page.locator('//p[text()="Verifique o número do pedido e tente novamente"]');
+    //const message =  page.locator('p', {hasText: 'Verifique o número do pedido e tente novamente'});
+    //await expect(message).toBeVisible();
+    
+    await expect(page.locator('#root')).toMatchAriaSnapshot(`
+        - img
+        - heading "Pedido não encontrado" [level=3]
+        - paragraph: Verifique o número do pedido e tente novamente
+        `);
+});
