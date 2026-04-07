9073dbbce9dceab5c05af307d34554c4c55e33e9
Otávio Carneiro Ribeiro <otavio@kyte.com.br>
2026-04-07 18:41:40 -0300

    Aula Validações de Interface Além do Tradicional com Snapshots finalizada

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -100,11 +100,39 @@ test.describe("Consultar Pedido", () => {
         await page.getByRole('textbox', { name: 'Número do Pedido' }).fill(order);
         await page.getByRole('button', { name: 'Buscar Pedido' }).click();
     
-        //Assert
-        await expect(page.getByTestId('order-result-id')).toContainText(order);
+        // //Assert
+        // await expect(page.getByTestId('order-result-id')).toContainText(order);
     
-        await expect(page.getByTestId('order-result-status')).toBeVisible();
-        await expect(page.getByTestId('order-result-status')).toContainText('APROVADO');
+        // await expect(page.getByTestId('order-result-status')).toBeVisible();
+        // await expect(page.getByTestId('order-result-status')).toContainText('APROVADO');
+        await expect(page.getByTestId(`order-result-${order}`)).toMatchAriaSnapshot(`
+            - img
+            - paragraph: Pedido
+            - paragraph: ${order}
+            - img
+            - text: APROVADO
+            - img "Velô Sprint"
+            - paragraph: Modelo
+            - paragraph: Velô Sprint
+            - paragraph: Cor
+            - paragraph: Lunar White
+            - paragraph: Interior
+            - paragraph: cream
+            - paragraph: Rodas
+            - paragraph: sport Wheels
+            - heading "Dados do Cliente" [level=4]
+            - paragraph: Nome
+            - paragraph: Otávio Ribeiro
+            - paragraph: Email
+            - paragraph: otavio@ribeiro.com
+            - paragraph: Loja de Retirada
+            - paragraph
+            - paragraph: Data do Pedido
+            - paragraph: /\\d+\\/\\d+\\/\\d+/
+            - heading "Pagamento" [level=4]
+            - paragraph: À Vista
+            - paragraph: /R\\$ \\d+\\.\\d+,\\d+/
+            `);
     });
     
     test('deve exibir mensagem quando o pedido não é encontrado', async ({ page }) => {
